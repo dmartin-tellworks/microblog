f485753f21a95a34f7bcbaf1b664df03b6038ee7
dmartin-tellworks <dmartin@tellworks.com>
2020-08-17 16:12:52 -0400

    added pagination for posts

--- config.py
+++ config.py
@@ -13,5 +13,7 @@ class Config(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSOWRD')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dmartin@tellworks.com']
+
+    POSTS_PER_PAGE = 3
