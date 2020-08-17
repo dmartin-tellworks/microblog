371f47c169a34bba83580c96338eabd606a63389
dmartin-tellworks <dmartin@tellworks.com>
2020-08-17 10:21:41 -0400

    Added error pages

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def index():
     return render_template('index.html', title='Home', posts=posts)
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
